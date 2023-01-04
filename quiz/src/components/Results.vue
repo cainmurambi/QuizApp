cca9db6ac48f34444d9675a42151e131477284b4
unknown <cainezekiel456@gmail.com>
2023-01-04 09:43:10 +0200

    adding the question and results component

--- quiz/src/components/Results.vue
+++ quiz/src/components/Results.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-
+    <div class="result">
+        <div class="title">You got sample result 1!</div>
+        <div class="desc">
+            Enter a short description here about the result.
+        </div>
     </div>
 </template>
 
